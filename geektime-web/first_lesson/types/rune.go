89ae4ec968d85f741413de4a5930dd51e1b2af1a
deen <adler.liu16@gmail.com>
2023-06-18 15:46:11 +0800

    Changes

---
+++ geektime-web/first_lesson/types/rune.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	var a byte = 13
+	var b rune = 13
+	println(a, b)
+}
--- geektime-web/main.go
+++ geektime-web/main.go
@@ -1,6 +1,12 @@
 package main
 
+//import _ "fmt" //匿名引用
+
 func main() {
 	println("hello go")
 
 }
+
+//func init() { //匿名引用就是为了执行这个方法的
+//
+//}
